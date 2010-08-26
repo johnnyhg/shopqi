c661e7973e15173c3da4e1218a4c4799f1442d95
po <mahb45@gmail.com>
2010-08-26 18:42:29 +0800

    slideshow captain support mouseover

--- public/javascripts/slideshow.js
+++ public/javascripts/slideshow.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {		
-    //Execute the slideShow, set 4 seconds for each images
-    slideShow(4000);
+    //Execute the slideShow, set 3 seconds for each images
+    slideShow(3000);
 });
 
 function slideShow(speed) {
@@ -13,6 +13,9 @@ function slideShow(speed) {
   //append a LI item to the UL list for displaying caption
   $('ul.slideshow').append('<li id="slideshow-caption" class="caption"><ul class="slideshow-caption-container"></ul></li>');
 
+  //Display the caption
+  $('#slideshow-caption').css({opacity: 0.7, top:355});
+
   //Get the caption of the first image from REL attribute and display it
   var slideshow_image_size = $('ul.slideshow li.slideshow_image').size();
   $('ul.slideshow li.slideshow_image').each(function(){
@@ -24,10 +27,8 @@ function slideShow(speed) {
     caption.append(text);
   });
 
-  //Display the caption
-  $('#slideshow-caption').css({opacity: 0.7, top:355});
-
   //Call the gallery function to run the slideshow	
+  gallery();
   var timer = setInterval('gallery()',speed);
 
   //pause the slideshow on mouse over
@@ -37,27 +38,42 @@ function slideShow(speed) {
     timer = setInterval('gallery()',speed);			
   });
 
+  //point to current
+  $('ul.slideshow .slideshow-caption-container li').mouseover(function(){
+    if(!$(this).hasClass('current')){
+      var index = $('ul.slideshow .slideshow-caption-container li').index($(this));
+      var next = $('ul.slideshow > li').eq(index);
+      moveTo(next);
+    }
+  });
 }
 
 function gallery() {
-  //if no IMGs have the show class, grab the first image
-  var current = ($('ul.slideshow li.show')?  $('ul.slideshow li.show') : $('#ul.slideshow li:first'));
+  moveTo(getNext());
+}
 
-  //Get next image, if it reached the end of the slideshow, rotate it back to the first image
-  var next = ((current.next().length) ? ((current.next().attr('id') == 'slideshow-caption')? $('ul.slideshow li:first') :current.next()) : $('ul.slideshow li:first'));
+//if no IMGs have the show class, grab the first image
+function getCurrent(){
+  return ($('ul.slideshow li.show') ?  $('ul.slideshow li.show') : $('#ul.slideshow li:first'));
+}
 
-  //Get next image caption
-  var title = next.find('img').attr('title');	
+//Get next image, if it reached the end of the slideshow, rotate it back to the first image
+function getNext(){
+  var current = getCurrent();
+  return ((current.next().length) ? ((current.next().attr('id') == 'slideshow-caption')? $('ul.slideshow li:first') :current.next()) : $('ul.slideshow li:first'));
+}
+
+function moveTo(next){
+  var current = getCurrent();
 
   //Set the fade in effect for the next image, show class has higher z-index
   next.css({opacity: 0.0}).addClass('show').animate({opacity: 1.0}, 1000);
 
-  //Display the content
-  $('.slideshow-caption-container li').removeClass('current').css({opacity: 0.7});
-  var index = $('ul.slideshow > li').index(next);
-  $('.slideshow-caption-container li:eq(' + index + ')').addClass('current').css({opacity: 1.0});
-
   //Hide the current image
   current.animate({opacity: 0.0}, 1000).removeClass('show');
 
+  //Display the content
+  $('.slideshow-caption-container li').removeClass('current').css({opacity: 0.7});
+  var index = $('ul.slideshow > li').index(next);
+  $('.slideshow-caption-container li').eq(index).addClass('current').css({opacity: 1.0});
 }
