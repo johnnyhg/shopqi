765e98e08b88a340bcfa0db2f4c7fc153018503f
po <mahb45@gmail.com>
2010-08-28 12:57:04 +0800

    show container tooltip at topRight use qtip

--- public/javascripts/admin/container_operates.js
+++ public/javascripts/admin/container_operates.js
@@ -52,9 +52,19 @@ var container_edit_tooltip_setting = $.extend({}, tooltip_setting, {
 });
 
 jQuery(function($) {
-  $('.container_operates').tooltip(container_operates_tooltip_setting).dynamic(dynamic_setting);
-  $('.container:empty').tooltip(container_new_tooltip_setting).dynamic(dynamic_setting);
-  $('.grid_1:empty, .grid_2:empty, .grid_3:empty, .grid_4:empty, .grid_5:empty, .grid_6:empty, .grid_7:empty, .grid_8:empty, .grid_9:empty, .grid_10:empty, .grid_11:empty, .grid_12:empty, .grid_13:empty, .grid_14:empty, .grid_15:empty, .grid_16:empty, .grid_17:empty, .grid_18:empty, .grid_19:empty, .grid_20:empty, .grid_21:empty, .grid_22:empty, .grid_23:empty, .grid_24:empty').tooltip(grid_new_tooltip_setting);
+  //$('.container_operates').tooltip(container_operates_tooltip_setting).dynamic(dynamic_setting);
+  //$('.container:empty').tooltip(container_new_tooltip_setting).dynamic(dynamic_setting);
+  $('.grid_1:empty, .grid_2:empty, .grid_3:empty, .grid_4:empty, .grid_5:empty, .grid_6:empty, .grid_7:empty, .grid_8:empty, .grid_9:empty, .grid_10:empty, .grid_11:empty, .grid_12:empty, .grid_13:empty, .grid_14:empty, .grid_15:empty, .grid_16:empty, .grid_17:empty, .grid_18:empty, .grid_19:empty, .grid_20:empty, .grid_21:empty, .grid_22:empty, .grid_23:empty, .grid_24:empty').each(function(){
+    var parent = $(this).parent('div[id]');
+    var parent_id = id(parent.attr('id'));
+    $(this).qtip({
+      content: { url: '/containers/new', data: { parent_id: parent_id }},
+      position: 'topRight',
+      hide: { fixed: true }, // Make it fixed so it can be hovered over
+      show: { delay: 800 },
+      style: { padding: '5px 10px', name: 'green' }
+    });
+  });
   $('.grid_1:empty, .grid_2:empty, .grid_3:empty, .grid_4:empty, .grid_5:empty, .grid_6:empty, .grid_7:empty, .grid_8:empty, .grid_9:empty, .grid_10:empty, .grid_11:empty, .grid_12:empty, .grid_13:empty, .grid_14:empty, .grid_15:empty, .grid_16:empty, .grid_17:empty, .grid_18:empty, .grid_19:empty, .grid_20:empty, .grid_21:empty, .grid_22:empty, .grid_23:empty, .grid_24:empty').each(function(){
     $(this).height($(this).prev().height());
   });
