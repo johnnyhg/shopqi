8f8bc2e17c9622d1a3f65f6237f26b6426d5df60
po <mahb45@gmail.com>
2010-12-06 16:47:32 +0800

    yeah! Fixed the wired problem: if browser focused, then its ok, but if not, category operation (like add, mofidy) will not post final data to server.

--- features/step_definitions/category_steps.rb
+++ features/step_definitions/category_steps.rb
@@ -50,8 +50,8 @@ end
 
 当 /^在分类名称输入框中输入(.+)/ do |value|
   find('.tree input:first').set(value)
-  # 点击其他节点，使输入框失焦点，并向后台提交信息
-  find('.tree').click
+  # 输入框失焦点，并向后台提交信息
+  page.execute_script('$(".tree input:first").blur();')
   # 等一会，让ajax后台提交数据
   sleep 2
 end
