c26716003cd89f68a4b4ee957b130faca4cd478a
saberma <mahb45@gmail.com>
2011-01-27 15:54:05 +0800

    cache test classes cause cucumber throws a lot of error.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,9 +5,7 @@ Shopqi::Application.configure do
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
-  #config.cache_classes = true
-  #spork reload class
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
