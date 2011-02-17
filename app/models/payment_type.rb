e8be07f310bb4a6433ec43dfcb318c20ae9db224
saberma <mahb45@gmail.com>
2011-02-17 16:06:51 +0800

    add payment apply link
    add show store page link.

--- app/models/payment_type.rb
+++ app/models/payment_type.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 class PaymentType < ActiveHash::Base
   self.data = [
-    {:id => 1, :name => '在线支付-支付宝'},
-    {:id => 2, :name => '在线支付-财付通'},
-    {:id => 3, :name => '在线支付-快钱'}
+    {:id => 1, :name => '在线支付-支付宝', :link => 'https://b.alipay.com/order/productSign.htm?action=newsign&productId=2011011904422299'},
+    {:id => 2, :name => '在线支付-财付通', :link => 'http://union.tenpay.com/mch/mch_register.shtml'},
+    {:id => 3, :name => '在线支付-快钱', :link => 'http://www.99bill.com'}
   ]
   attr_accessor :payment
   delegate :is_show, :account, :partnerid, :verifycode, :remark, :to => :payment, :allow_nil => true
