2f3705ca1238bd9c1d59cc571cc5b4a073d8af3e
saberma <mahb45@gmail.com>
2011-02-17 21:30:38 +0800

    update store title.

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -1,4 +1,13 @@
+# encoding: utf-8
 module StoresHelper
+  def platform
+    store.name.blank? ? 'ShopQi工作平台' : "#{store.name}工作平台"
+  end
+
+  def store_title
+    store.title.blank? ? '请在[后台管理]-[设置]中输入标题' : store.title
+  end
+
   def config_store_url
     "#{show_store_url}?config=true"
   end
