9dbdaa316aa1c76fb6bcb605e8b5bc678ed9219a
saberma <mahb45@gmail.com>
2011-02-17 20:04:13 +0800

    add store url method.

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -1,2 +1,13 @@
 module StoresHelper
+  def config_store_url
+    "#{show_store_url}?config=true"
+  end
+
+  def show_store_url
+    "#{request.protocol}#{store.subdomain}.#{request.domain}#{request.port_string}"
+  end
+
+  def admin_store_url
+    "#{show_store_url}/admin"
+  end
 end
