b1d9f6cda6391082f68f3669119e6ef362ea150f
saberma <mahb45@gmail.com>
2011-04-08 23:16:13 +0800

    add last class for help.

--- app/helpers/helps_helper.rb
+++ app/helpers/helps_helper.rb
@@ -1,2 +1,6 @@
 module HelpsHelper
+  def last(collection, item, class_name = :last)
+    return '' if collection.nil?
+    (item == collection.last) ? class_name : ''
+  end
 end
--- app/helpers/navs_helper.rb
+++ app/helpers/navs_helper.rb
@@ -1,4 +1,5 @@
 module NavsHelper
+  #TODO: change to first
   def bar(collection, item, class_name = :first)
     return '' if collection.nil?
     (item == collection.first) ? class_name : ''
